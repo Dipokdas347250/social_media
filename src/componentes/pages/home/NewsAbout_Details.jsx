fedadaeb7f7cd1d435ac286bde5d3cdf67bf08ec
Dipokdas347250 <156934776+Dipokdas347250@users.noreply.github.com>
2026-03-13 03:07:07 +0600

    navber , sideber and rightber add in projuct

---
+++ src/componentes/pages/home/NewsAbout_Details.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewsAbout_Details = () => {
+  return (
+    <div>NewsAbout_Details</div>
+  )
+}
+
+export default NewsAbout_Details
--- src/componentes/pages/home/Newsfeed.jsx
+++ src/componentes/pages/home/Newsfeed.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NewsFeedTop from '../home/NewsfeedTop'
+import NewsAbout_Details from './NewsAbout_Details'
 
 const Newsfeed = () => {
   return (
     <>
      <NewsFeedTop/>
+     <NewsAbout_Details/>
     </>
   )
 }
