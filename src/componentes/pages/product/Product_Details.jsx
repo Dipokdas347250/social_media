a5df02d1f29a8cb75fe12564862def4d25cba7d2
Dipokdas347250 <dipakdas24680@gmail.com>
2026-03-18 00:24:43 +0600

    MERNIAN Marketplace pregetion done

--- src/componentes/pages/product/Product_Details.jsx
+++ src/componentes/pages/product/Product_Details.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Product_Details = () => {
   return (
-    <div>
-      idhgfkaslg
-    </div>
+   <>
+   <div className=" mt-6">
+    <h3 className='text-[14px] text-[#AFB0C0] font-normal font-poppins'>BROWSE PRODUTS</h3>
+    <h2 className='text-[24px] text-[#24234B] font-medium font-poppins'>Digital Items</h2>
+   </div>
+   </>
   )
 }
 
