fd9f679e7768254b7e8da78994aa72de4d77b3ec
Dipokdas347250 <dipakdas24680@gmail.com>
2026-03-21 19:54:40 +0600

    MERNIAN Marketplace pregetion done

--- src/componentes/pages/product/Product_Details.jsx
+++ src/componentes/pages/product/Product_Details.jsx
@@ -7,6 +7,18 @@ const Product_Details = () => {
     <h3 className='text-[14px] text-[#AFB0C0] font-normal font-poppins'>BROWSE PRODUTS</h3>
     <h2 className='text-[24px] text-[#24234B] font-medium font-poppins'>Digital Items</h2>
    </div>
+   <div className=" mt-6">
+    <h3 className='text-[14px] text-[#AFB0C0] font-normal font-poppins'>BROWSE PRODUTS</h3>
+    <h2 className='text-[24px] text-[#24234B] font-medium font-poppins'>Digital Items</h2>
+   </div>
+   <div className=" mt-6">
+    <h3 className='text-[14px] text-[#AFB0C0] font-normal font-poppins'>BROWSE PRODUTS</h3>
+    <h2 className='text-[24px] text-[#24234B] font-medium font-poppins'>Digital Items</h2>
+   </div>
+   <div className=" mt-6">
+    <h3 className='text-[14px] text-[#AFB0C0] font-normal font-poppins'>BROWSE PRODUTS</h3>
+    <h2 className='text-[24px] text-[#24234B] font-medium font-poppins'>Digital Items</h2>
+   </div>
   
    </>
   )
